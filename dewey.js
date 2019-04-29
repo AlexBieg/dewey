86fa9e95bfc15e8a04e9a9acc976521530e1165b
AlexBieg <alexbieg95@gmail.com>
2019-04-29 11:00:05 -0700

    Got basic tests working

--- dewey.js
+++ dewey.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import chalk from 'chalk';
+import path from 'path';
+import { get as _get } from 'lodash';
 
 class Dewey {
   constructor(dir, config) {
@@ -22,18 +24,83 @@ class Dewey {
    * Test all files using the provided directory and config
    */
   test() {
-    let files = fs.readdirSync(this.dir);
+    this.testDir(this.dir, this.config, []);
+  }
 
-    while (files.length > 0) {
-      if (this.config.test.files.includes(files[0])) {
-        console.log(chalk.green('✓', files[0]));
-        this.successes.push(files[0]);
-      } else {
-        console.error(chalk.red('𝘅', files[0]));
-        this.errors.push(files[0])
+  testDir(dir, config, pathToDir) {
+    const names = fs.readdirSync(path.join(...pathToDir, dir));
+    const { files, dirs } = names.reduce((acc, name) => {
+      const stat = fs.lstatSync(path.join(...pathToDir, dir, name));
+      if (stat.isFile()) {
+        acc.files.push(name);
+      } else if (stat.isDirectory()) {
+        acc.dirs.push(name);
       }
-      files = files.slice(1, files.length);
+
+      return acc;
+    }, { files: [], dirs: [] });
+
+    files.forEach((file) => {
+      if (this.matchIgnore(file, config)) {
+        console.log(chalk.gray('◉', file))
+      } else if (this.matchFile(file, config)) {
+        console.log(chalk.green('✓', file));
+        this.successes.push(file);
+      } else {
+        console.error(chalk.red('𝘅', file));
+        this.errors.push(file)
+      }
+    });
+
+    dirs.forEach((childDir) => {
+      if (this.matchIgnore(childDir, config)) {
+        console.log(chalk.yellow('◉', childDir))
+      } else {
+        const dirConfig = this.getConfigForDir(childDir, config, dir);
+        if (!dirConfig) {
+          console.error(chalk.red('𝘅', childDir));
+          this.errors.push(childDir)
+        } else {
+          console.log(chalk.green('✓', childDir));
+          this.successes.push(childDir);
+          this.testDir(childDir, dirConfig, [...pathToDir, dir]);
+        }
+      }
+    })
+  }
+
+  matchIgnore(name, config) {
+    return _get(config, 'ignore', []).some((matcher) => {
+      return this.match(name, matcher);
+    })
+  }
+
+  matchFile(file, config, dir) {
+    return _get(config, 'files', []).some((matcher) => {
+      return this.match(file, matcher, dir);
+    })
+  }
+
+  match(name, matcher, dir) {
+    if (typeof matcher === 'string') {
+      return matcher === name;
+    } else if (typeof matcher === 'function') {
+      return matcher(name, dir)
+    } else if (matcher instanceof RegExp) {
+      return name.match(matcher);
     }
+  }
+
+  getConfigForDir(dir, parentConfig, parentDir) {
+    let config;
+
+    _get(parentConfig, 'dirs', []).forEach((dirConfig) => {
+      if (this.match(dir, dirConfig.dirName, parentDir)) {
+        config = dirConfig.config;
+      }
+    })
+    
+    return config;
   }
 
   /**
--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 #!/usr/bin/env node
 import fs from 'fs';
 import Dewey from './dewey.js';
+import path from 'path';
 
 const [,, ...args] = process.argv;
 const [argsStartingDir, argsConfigPath] = args;
-const configPath = argsConfigPath || './.deweyrc';
+const configPath = argsConfigPath || './dewey.config.js';
 const startingDir = argsStartingDir || './';
-
-let config;
-try {
-  config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
-} catch (e) {
-  console.error('\x1b[31m', 'No/Invalid dewey config file found');
-  process.exit();
-}
 
 let dir;
 try {
   dir = fs.readdirSync(startingDir);
 } catch (e) {
   console.error('\x1b[31m', 'Unable to find starting directory');
+  process.exit();
+}
+
+let config;
+try {
+  config = require(path.resolve(process.cwd(), configPath));
+} catch (e) {
+  console.error('\x1b[31m', 'No/Invalid dewey config file found');
   process.exit();
 }
 
